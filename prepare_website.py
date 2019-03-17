86807dc27f9d8f2afb9b9739ea50a6a366fd985f
Milos Vasic <milos85vasic@gmail.com>
2019-03-17 10:04:53 +0100

    Apache Factory, Wordpress Matrix - Website prepare.

--- prepare_website.py
+++ prepare_website.py
@@ -1,8 +1,14 @@
 from configuration import *
+from Setup.Tools.commands import *
 
 
 def prepare():
     print("Preparing Wordpress installation.")
+    steps = [
+
+    ]
+
+    run(steps)
 
 
 if __name__ == '__main__':
