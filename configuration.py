54a930b9f392930b6ce148c8cfefc19038f11377
Milos Vasic <milos85vasic@gmail.com>
2019-03-17 09:52:09 +0100

    Apache Factory, Wordpress Matrix - Prepare script.

--- configuration.py
+++ configuration.py
@@ -1,1 +1,2 @@
 download_url = "https://wordpress.org/wordpress-5.1.1.tar.gz"
+
---
+++ prepare.py
@@ -0,0 +1,9 @@
+from configuration import *
+
+
+def prepare():
+    print("Preparing Wordpress installation.")
+
+
+if __name__ == '__main__':
+    prepare()
